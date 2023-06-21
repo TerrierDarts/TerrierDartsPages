b1729607d0c271e9646aa89b037db01d5ff2cc7f
TerrierDarts <terrierdarts@gmail.com>
2023-06-21 22:29:58 +0100

    External Link Added and Handled and Nav Formatted

--- src/consts.ts
+++ src/consts.ts
@@ -30,7 +30,7 @@ export const GITHUB_EDIT_URL = ``;
 
 export const COMMUNITY_INVITE_URL = ``;
 
-// See "Algolia" section of the README for more information.
+//See "Algolia" section of the README for more information.
 export const ALGOLIA = {
   indexName: 'TD_Search',
   appId: 'G0IGEJ4ZKT',
@@ -40,12 +40,14 @@ export const ALGOLIA = {
 export interface NavLink {
   text: string;
   link: string;
+
 }
 
 export interface NavGroup {
   text: string;
   children?: (NavLink | NavGroup)[];
   link?: string;
+
 }
 
 
@@ -56,71 +58,86 @@ export type Sidebar = Record<
 
 export const SIDEBAR: Sidebar = {
   en: {
-    Pages: { 
-      text: '', 
-      children: 
-      [
-      { 
-        text: 'General', 
-        children: [
-          {text: 'Home', link: '../en/home' },
-          {text: 'Podcasts', link: '../en/home' },
-          {text: 'Streamer.bot', link: '../en/home' },
-          {text: 'Extensions', link: '../en/home' },
-      ] 
-      },
-      {
-        text: 'Builds',
-        children: [
-
+    Pages: {
+      text: '',
+      children:
+        [
           {
-             text: 'General',
-             children: [
-              { text: 'Custom Welcomes', link: '../en/welcome_users' },
-              { text: 'Follow Age Responses', link: '../en/follow_age_responses' },
-              { text: 'Per Game Death Counter', link: '../en/death_counter_multi_game'},
-              { text: 'Gameboy Game Changer', link: '../en/gameboy_game_change'},
-              { text: 'Random Game Changer', link: '../en/random_game_change'},
-          ]
+            text: 'General',
+            children: [
+              { text: 'Home', link: '../en/home' },
+              { text: 'Podcasts', link: '../en/podcasts' },
+              { text: 'Streamer.bot', link: 'https://streamer.bot' },
+              { text: 'Extensions', link: 'https://extensions.streamer.bot' },
+            ]
           },
-
           {
-          text: 'Heat',
-          children: [
-           { text: 'Heat - Core', link: '../en/heat_core_ws5' },
-           //{ text: 'Heat - Mario', link: '../en/heat_mario' },
-           // { text: 'Heat - Click Map', link: '../en/heat_click_map' },
-           // { text: 'Heat - Offline Test', link: '../en/heat_offline_test' },
-          ],
-          },
+            text: 'Builds',
+            children: [
 
-          {
-          text: 'Point System Games',
-          children: [
-          //{ text: 'Slots', link: '../en/slots' },
-          { text: 'Gamble', link: '../en/gamble' },
-          // { text: 'Casino Roulette', link: '../en/casino_roulette' },
-          // { text: 'Lotto', link: '../en/lotto' },
-          // { text: 'Heists', link: '../en/heists' },
-          // { text: 'Boss Battles', link: '' },
-          // { text: 'Raffles', link: '../en/raffles' },
-          // { text: 'Free-For-Alls', link: '../en/free_for_all' },
-          // { text: 'Dice Game', link: '' },
-          ],
-          },
+              {
+                text: 'General',
+                children: [
+                  { text: 'Custom Welcomes', link: '../en/welcome_users' },
+                  { text: 'Follow Age Responses', link: '../en/follow_age_responses' },
+                  { text: 'Per Game Death Counter', link: '../en/death_counter_multi_game' },
+                  { text: 'Gameboy Game Changer', link: '../en/gameboy_game_change' },
+                  { text: 'Random Game Changer', link: '../en/random_game_change' },
+                ]
+              },
 
-          {
-          text: 'Chat Based Games',
-          children: [
-           { text: 'Cluedo', link: '' },
-           // { text: 'Snakes and Ladders', link: '' },
-           // { text: 'Hangman', link: '' },
-           // { text: 'Word Wheel', link: '' },
-          ],
+              {
+                text: 'Heat',
+                children: [
+                  { text: 'Heat - Core', link: '../en/heat_core_ws5' },
+                  { text: 'Heat - Mario', link: '../en/heat_mario' },
+                  { text: 'Heat - Click Map', link: '../en/heat_click_map' },
+                  { text: 'Heat - Offline Test', link: '../en/heat_offline_test' },
+                ],
+              },
+
+              {
+                text: 'Point System Add-Ons',
+                children: [
+                  { text: 'Slots', link: '../en/slots' },
+                  { text: 'Gamble', link: '../en/gamble' },
+                  { text: 'Casino Roulette', link: '../en/casino_roulette' },
+                  { text: 'Lotto', link: '../en/lotto' },
+                  { text: 'Heists', link: '../en/heists' },
+                  { text: 'Boss Battles', link: '' },
+                  { text: 'Raffles', link: '../en/raffles' },
+                  { text: 'Free-For-Alls', link: '../en/free_for_all' },
+                  { text: 'Dice Game', link: '' },
+                ],
+              },
+              {
+                text: 'Ranking System',
+                children: [
+                  { text: 'Slots', link: '../en/slots' },
+                  { text: 'Gamble', link: '../en/gamble' },
+                  { text: 'Casino Roulette', link: '../en/casino_roulette' },
+                  { text: 'Lotto', link: '../en/lotto' },
+                  { text: 'Heists', link: '../en/heists' },
+                  { text: 'Boss Battles', link: '' },
+                  { text: 'Raffles', link: '../en/raffles' },
+                  { text: 'Free-For-Alls', link: '../en/free_for_all' },
+                  { text: 'Dice Game', link: '' },
+                ],
+              },
+
+
+              {
+                text: 'Chat Based Games',
+                children: [
+                  { text: 'Cluedo', link: '' },
+                  { text: 'Snakes and Ladders', link: '' },
+                  { text: 'Hangman', link: '' },
+                  { text: 'Word Wheel', link: '' },
+                ],
+              },
+            ],
           },
-      ],
-   },
-  ],
-  },
+        ],
+    },
   }
 }
